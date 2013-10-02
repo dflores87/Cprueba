6179069f6496fbf9413132fbe97c7070424a1a0e
Daniel Flores <danielf_2008@hotmail.com>
2013-10-02 14:06:27 -0300

    subiendo librerias comunes

---
+++ holaoperativo/Debug/src/holaoperativo.d
@@ -0,0 +1,4 @@
+src/holaoperativo.d: ../src/holaoperativo.c \
+ /home/utnso/Escritorio/desarrollo/commons/commons/temporal.h
+
+/home/utnso/Escritorio/desarrollo/commons/commons/temporal.h:
---
+++ holaoperativo/src/holaoperativo.c
@@ -0,0 +1,20 @@
+/*
+ ============================================================================
+ Name        : holaoperativo.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <commons/temporal.h>
+
+int main(void) {
+	char* tiempo =temporal_get_string_time();
+	puts(tiempo);
+	free(tiempo);
+	return EXIT_SUCCESS;
+}
